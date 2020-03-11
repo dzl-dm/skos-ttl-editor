48c80bcedf6cf3a6b394ec8103d455cd0e79a7a8
Stöhr <stmar7@gifo.net>
2020-03-11 14:53:42 +0100

    Implemented unit test for hierarchy diagnostic.

---
+++ src/test/suite/semantichandler.test.ts
@@ -0,0 +1,29 @@
+import * as assert from 'assert';
+
+import * as vscode from 'vscode';
+import * as parser from '../../parser';
+import * as documenthandler from '../../documenthandler';
+import * as subjecthandler from '../../subjecthandler';
+import * as path from 'path';
+import { SemanticHandler } from '../../semantichandler';
+
+suite('Parser Test Suite', () => {
+    vscode.window.showInformationMessage('Start all tests.');
+
+    let uri = vscode.Uri.file(path.join(__dirname,'../../../src/test/test.ttl'));
+
+    test('Hierarchy diagnostic test', async () => {
+        await new documenthandler.DocumentHandler({}).openTextDocument(uri).then(doc => {
+            let sss = new parser.SkosParser().parseTextDocument(doc)||{};
+            let semantichandler = new SemanticHandler();
+            semantichandler.checkSemantics(sss);
+            let diagnostics = semantichandler.diagnosticCollection.get(uri);  
+            assert.ok(diagnostics?.filter(d => {
+                return d.message === "Hierarchical recursion: <http://data.dzl.de/ont/dwh#TestResource2111>,<http://data.dzl.de/ont/dwh#TestResource211>,<http://data.dzl.de/ont/dwh#TestResource21>";
+            }).length === 3);
+            assert.ok(diagnostics?.filter(d => {
+                return d.message.startsWith("Hierarchical recursion: ");
+            }).length === 3);
+        });
+    });
+});
